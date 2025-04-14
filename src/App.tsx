e07bf8f1cbead6085354f9c496c83cb3f29cf20d
Simo-Pekka Leppänen <simo-pekka.leppanen@kipina.fi>
2025-04-14 15:34:07 +0300

    Implement a working example with a hook that returns everything related to odd mode

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,35 @@ import { OddModeAnimation } from "./OddModeAnimation";
 import { log } from "./utils";
 
 function App() {
-  const { data, isLoading } = useOddMode();
+  const [{ isOddMode, oddModeChanged, isLoading, isError }, { enterOddMode }] = useOddMode();
   // Demonstrate a modal dialog that's in the DOM
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    if (data?.changed && !data.oddMode) {
-      log("show modal");
-      setShowModal(true);
+    if (oddModeChanged) {
+      if (!isOddMode) {
+        log("show modal");
+        setShowModal(true);
+      } else {
+        enterOddMode();
+      }
     }
-  }, [data]);
+  }, [enterOddMode, isOddMode, oddModeChanged]);
 
   if (isLoading) {
     return <div>Loading odd mode...</div>;
   }
 
-  if (!data) {
+  if (isError) {
     return <div>error...</div>;
   }
 
-  const { oddMode, changed } = data;
-
   return (
     <>
-      <OddModeAnimation oddMode={oddMode} />
+      <OddModeAnimation oddMode={isOddMode} />
       <h1>Odd mode example</h1>
-      <p>Odd mode is {oddMode ? "ON" : "OFF"}</p>
-      <p>Changed in last API call: {changed.toString()}</p>
+      <p>Odd mode is {isOddMode ? "ON" : "OFF"}</p>
+      <p>Changed in last API call: {oddModeChanged.toString()}</p>
       {showModal && (
         <div className="modal">
           <div className="modal-content">
--- src/queries.ts
+++ src/queries.ts
@@ -1,43 +1,68 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { apiIsOddity, getOddModeState, saveOddModeState } from "./api";
 import { log, sleep } from "./utils";
+import { useCallback } from "react";
 
-/** Return whether odd mode is on and whether the state has changed */
-export const useOddMode = () => {
-  const enterMutation = useEnterOddMode();
-
-  return useQuery({
-    queryKey: ["useIsInOddMode"],
-    queryFn: async () => {
-      const oddMode = await apiIsOddity();
-      const last = await getOddModeState();
-      const changed  = oddMode !== last;
-      if (changed) {
-        if (oddMode) {
-          enterMutation.mutate(); // Run in background
-        }
-        await saveOddModeState(oddMode);
-      }
-
-      return { oddMode, changed };
-    },
-    refetchInterval: 1000 * 5,
-  });
+type OddModeState = {
+  isOddMode: boolean;
+  oddModeChanged: boolean;
+  isLoading: boolean;
+  isError: boolean;
 };
 
-/** Mutation that should be run when entering odd mode */
-export const useEnterOddMode = () => {
-  return useMutation({
-    mutationFn: async () => {
-      log("entering odd mode...");
-      await sleep(1000);
-      log("this setup is gonna take...");
-      await sleep(1000);
-      log("a while...");
-      await sleep(2000);
-      // window.alert("Odd mode is on!"); // We can make API calls from mutations but not modify the DOM
-      await sleep(2000);
-      log("done!");
+type OddModeMutation = {
+  enterOddMode: () => void;
+};
+
+export function useOddMode(): [
+  OddModeState,
+  OddModeMutation
+] {
+  /** Return whether odd mode is on and whether the state has changed */
+  const { data, isLoading, isError } = useQuery({
+      queryKey: ["useIsInOddMode"],
+      queryFn: async () => {
+        const oddMode = await apiIsOddity();
+        const last = await getOddModeState();
+        const changed  = oddMode !== last;
+        if (changed) {
+          await saveOddModeState(oddMode);
+        }
+
+        return { isOddMode: oddMode, oddModeChanged: changed };
+      },
+      refetchInterval: 1000 * 5,
+    });
+  
+
+  /** Mutation that should be run when entering odd mode */
+  const enterOddMode = useMutation({
+      mutationFn: async () => {
+        log("entering odd mode...");
+        await sleep(1000);
+        log("this setup is gonna take...");
+        await sleep(1000);
+        log("a while...");
+        await sleep(2000);
+        // window.alert("Odd mode is on!"); // We can make API calls from mutations but not modify the DOM
+        await sleep(2000);
+        log("done!");
+      },
+    });
+
+  /** Memoize the mutation function to avoid re-renders. The function itself
+   * does not change so it can be memoized with an empty dependency array.
+   */
+  const memoedEntranceFn = useCallback(enterOddMode.mutate, []);
+  return [
+    {
+      isOddMode: data?.isOddMode || false,
+      oddModeChanged: data?.oddModeChanged || false,
+      isLoading,
+      isError,
     },
-  });
+    {
+      enterOddMode: memoedEntranceFn,
+    }
+  ]
 }
